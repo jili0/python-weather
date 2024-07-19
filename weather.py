0f2d9aefb7bd99afcf053a32296f4b45a18faf38
Jing Li <jingli0319@gmail.com>
2024-07-19 11:32:51 +0200

    first commit

---
+++ weather.py
@@ -0,0 +1,29 @@
+from pprint import pprint
+import os
+from dotenv import load_dotenv
+import requests
+
+
+load_dotenv()
+
+def get_current_weather(city="Berlin"):
+  request_url = f'https://api.openweathermap.org/data/2.5/weather?&appid={os.getenv("API_KEY")}&q={city}&units=metric'
+  
+  weather_data = requests.get(request_url).json()
+
+  return weather_data
+
+if __name__ == "__main__":
+  print("\n*** Get Current Weather Conditions ***\n")
+
+  city = input("\n Please enter a city name:\n")
+
+  if not bool(city.strip()):
+    city = "Berlin"
+
+  weather_data = get_current_weather(city)
+
+  print("\n")
+  pprint(weather_data)
+
+
